8d00e8f4697f20e6a78e347799d455ce4f32cda0
Coder2100 <apexcurestats@gmail.com>
2019-07-11 02:53:55 +0200

    Images

---
+++ 18_2_Stringers/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type IPAddr [4]byte
+
+// add a string() string methods to IPAdress
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
